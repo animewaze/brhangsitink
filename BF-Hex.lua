e9bf1b660dcd91c8c19a80805a37f3ceda253360
animewaze <gabrielmagaess39ilva@gmail.com>
2026-09-06 21:32:54 -0300

    Update script to load new version and team settings

--- BF-Hex.lua
+++ BF-Hex.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/8QbjQhnL"))()
+getgenv().Version = "V2"; getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DEV-HEX-HUB/Hex/refs/heads/main/DEVHEXLUBNII"))()
